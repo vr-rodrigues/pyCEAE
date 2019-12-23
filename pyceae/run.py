540ffe757aa0c71f76f35e192da10e55c2904cb0
victorrangelrod <39785146+victorrangelrod@users.noreply.github.com>
2019-12-23 05:44:30 -0300

    Add files via upload

--- pyceae/run.py
+++ pyceae/run.py
@@ -22,16 +22,16 @@ base_atualizada.drop('index', axis=1, inplace=True)
 # Calculo Inflação e Atualização
 # ===========================================
 
-if __name__ == '__main__':
-    # Resultados Item
-    inflacao_item = item_inflation(base_atualizada)
-    inflacao_item = inflacao_item.melt(id_vars=['data']).round(decimals=3)
 
-    inflacao_item.to_csv('inflacao_item.csv', encoding='utf-16', sep=';', index=False, header=True, decimal=',')
-    print('\nArquivo inflacao_item.csv atualizado')
+# Resultados Item
+inflacao_item = item_inflation(base_atualizada)
+inflacao_item = inflacao_item.melt(id_vars=['data']).round(decimals=3)
 
-    # Resultados Cestas
-    inflacao_cesta = basket_inflation(base_atualizada)
+inflacao_item.to_csv('inflacao_item.csv', encoding='utf-16', sep=';', index=False, header=True, decimal=',')
+print('\nArquivo inflacao_item.csv atualizado')
 
-    inflacao_cesta.to_csv('inflacao_cesta.csv', encoding='utf-16', sep=';', index=False, header=True, decimal=',')
-    print("Arquivo inflacao_cesta.csv atualizado")
+# Resultados Cestas
+inflacao_cesta = basket_inflation(base_atualizada)
+
+inflacao_cesta.to_csv('inflacao_cesta.csv', encoding='utf-16', sep=';', index=False, header=True, decimal=',')
+print("Arquivo inflacao_cesta.csv atualizado")
